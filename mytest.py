abdb796372bdc5443acd95870a277d102c334dae
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-07-07 16:26:38 +0900

    Added dummy test.

--- mytest.py
+++ mytest.py
@@ -1,1 +1,12 @@
+import unittest
 
+def suite():
+    test_suite = unittest.TestSuite()
+    all_test_suite = unittest.defaultTestLoader.discover('saivdr.testcase', pattern='*.py')
+    for ts in all_test_suite:
+        test_suite.addTest(ts)
+    return test_suite
+
+if __name__ == "__main__":
+    mySuite = suite()
+    unittest.TextTestRunner().run(mySuite)
