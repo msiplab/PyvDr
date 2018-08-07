1c4a672f3f7a85f1d0d1ff4ca425c29b851e530e
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-08-08 07:08:33 +0900

    Modified files for setup.

--- setup.py
+++ setup.py
@@ -2,4 +2,12 @@ from setuptools import setup
 
 setup(
     name='saivdr',
+    version='0.0.1',
+    description='SaivDr package for Python',
+    long_description="""
+    Sparsity-aware image and volumetric data restoration (SaivDr) package for Python
+    """,
+    install_requires=[
+        'numpy',
+    ]
 )
