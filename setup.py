c2401ef3f1b7d053745047df942be85d11969377
Shogo Muramatsu <shogo@eng.niigata-u.ac.jp>
2018-08-09 22:43:53 +0900

    Revised to use setuptools.

--- setup.py
+++ setup.py
@@ -1,13 +1,46 @@
 from setuptools import setup
+import os
 
+def get_version(version_tuple):
+    if not isinstance(version_tuple[-1], int):
+        return '.'.join(
+            map(str, version_tuple[:-1])
+        ) + version_tuple[-1]
+    return '.'.join(map(str, version_tuple))
+
+init = os.path.join(
+    os.path.dirname(__file__), 'src', 'saivdr', '__init__.py'
+)
+
+def strip_comments(l):
+    return l.split('#', 1)[0].strip()
+
+def reqs(*f):
+    return list(filter(None, [strip_comments(l) for l in open(
+            os.path.join(os.getcwd(), *f)).readlines()]))
+
+version_line = list(
+    filter(lambda l: l.startswith('VERSION'), open(init))
+)[0]
+
+try:
+    from pypandoc import convert
+    def read_md(f):
+        return convert(f, 'rst')
+except ImportError:
+    convert = None
+    print(
+        'warning: pypandocモジュールが見つからないため、MarkdownからRSTに変換できません'
+    )
+    def read_md(f):
+        return open(f, 'r').read()
+
+README = os.path.join(os.path.dirname(__file__), 'README.md')
+VERSION = get_version(eval(version_line.split('=')[-1]))
 setup(
     name='saivdr',
-    version='0.0.1',
+    version=VERSION,
     description='SaivDr package for Python',
-    long_description="""
-    Sparsity-aware image and volumetric data restoration (SaivDr) package for Python
-    """,
-    install_requires=[
-        'numpy',
-    ]
+    long_description=read_md(README),
+    install_requires=reqs('requirements.txt')
 )
---
+++ src/saivdr/__init__.py
@@ -0,0 +1,2 @@
+VERSION = (0, 0, 1)
+__versin__ = '.'.join([str(x) for x in VERSION])
